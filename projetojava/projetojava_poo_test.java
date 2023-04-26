d49aea5ac2bd0ca179ee5c9fcaac81d4be87b5c0
Bianca Oliveira <babianca.sete@gmail.com>
2023-04-26 11:55:41 -0300

    version 1

--- projetojava/projetojava_poo_test.java
+++ projetojava/projetojava_poo_test.java
@@ -8,27 +8,47 @@ public class projetojava_poo_test {
 		
 		Scanner leia = new Scanner(System.in);
 		
-		String nome;
-		int idade;
-	    float celular, rg;
-		boolean Particular, Planosaude;
+		String nome, cpf,rg;
+		int idade, x = 0,op;
+	    float celular;
+		boolean Particular, Planosaude, comfirmação;
 		
 		paciente[] lista = new paciente[31];
 		
+		do {
+			System.out.println("Clinica José Marcondes");
+			System.out.println("Menu");
+			System.out.println("1- Marcar consulta");
+			System.out.println("2- Desmarcar consulta ");
+			System.out.println("3- Ver especialidades da Clínica");
+			System.out.println("0- Sair");
+			System.out.println("\n");
+			System.out.println("Digite sua opção: ");
+			op = leia.nextInt();
+			System.out.println("\n");
+			
+			switch(op) {
+			case 1:
 		System.out.println("Insira seu nome: ");
 		nome =leia.nextLine();
 		System.out.println("Insira sua idade: ");
 		idade =leia.nextInt();
 		System.out.println("Insira sua identidade: ");
-		rg =leia.nextInt();
+		rg =leia.nextLine();
+		System.out.println("Insira seu CPF: ");
+		cpf =leia.nextLine();
 		System.out.println("Vai dar entrada com o plano de saúde? \n(true para sim e false para não) ");
 		Planosaude =leia.hasNext();
 		System.out.println("Vai dar entrada com atendimento particular? \n(true para sim e false para não) ");
 		Particular =leia.hasNext();
 		System.out.println("Insira um telefone para contato: +55 11 ");
 		celular =leia.nextInt();
+		System.out.println("\n"); 
 		
-		lista[0] = new paciente (nome,idade,rg,Planosaude,Particular,celular);
+		lista[x] = new paciente (nome,idade,rg,cpf,Planosaude,Particular,celular);
+		break;
+	    }
+		}while(op!=0);
 	}
 
 }
